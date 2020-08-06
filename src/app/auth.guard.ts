befa993826993f092a8bdda02bbe4326c397506a
esscott1 <ericscott411@gmail.com>
2020-08-06 11:14:05 -0600

    secure profile app with upload

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanActivateChild, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, Router, CanActivateChild, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+
+import { Auth } from 'aws-amplify';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+  constructor(private router: Router) {}
+  canActivate():Promise < boolean > {
+    return new Promise((resolve) => {
+      Auth.currentAuthenticatedUser({
+          bypassCache: false
+        })
+        .then((user) => {
+          if(user){
+            resolve(true);
+          }
+        })
+        .catch(() => {
+          this.router.navigate(['/login']);
+          resolve(false);
+        });
+    });
   }
+
   canActivateChild(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
